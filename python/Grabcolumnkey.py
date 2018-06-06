471e8f1d30d8c2aeb5c9812d7bd646699c6455e9
muxiaofefei <33117451+muxiaofefei@users.noreply.github.com>
2018-06-06 09:31:30 +0800

    只抓取特定的客户端的key

--- python/Grabcolumnkey.py
+++ python/Grabcolumnkey.py
@@ -1,4 +1,5 @@
 # 自动抓取栏目key值
+#只抓取特定的客户端的key
 
 import sys
 import io
@@ -6,37 +7,90 @@ from urllib import request
 import re
 from bs4 import BeautifulSoup
 from xpinyin import Pinyin
+import requests
 
-# cookie数据
-cookie_str = input("请输入cookie：")
-# cookie_str = r'_csrf=aa1943b556d809619e55eaabb2a123c575742288f190b16263f7e74a70dfc1f9a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22d%B4%F5b%2B%1F%B0BN%19gt%99%09M%B4%ACzL%BFH+%D7%836%DC%8FQ%A7%88%D3%D1%22%3B%7D; CONTAINERID=20f731794e516105f4e374d8c3fa8215782133e2ce2ffe1450374d1317ca3f0e; RMTSESSION_FRONTEND=41cf2a16b85c4a1ab13f01a8bf8600b2; _identity=fefc94305d7bc3d4e9737b3018249b747cd5a5695adec09522dd0921a9004876a%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A51%3A%22%5B%22admin%40langxi.local%22%2C%22admin%40langxi.local%22%2C2592000%5D%22%3B%7D'
 
-# 访问在地址
-url = input("请输入网址：")
-# url = 'http://mp.rmt.test.routeryuncs.com/client/vchannel?module_id=5ac4295575c3a414008b4567'
+# username = 'admin@jrlx.com'
+# password = '59002e94532d7'
+# state = '0'
 
-req = request.Request(url)
-#设置cookie
-req.add_header('cookie', cookie_str)
-#设置请求头
-req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36')
+state = input("正式输1，测试输0：")
+username = input("请输入账号：")
+password = input("请输入密码：")
+cstkey = input("请输入客户端的key值：")
 
-resp = request.urlopen(req)
+if state == '1':
+    loginUrl = 'http://mp.tmtsp.com'
+else:
+    loginUrl = 'http://mp.rmt.test.routeryuncs.com'
 
-htmlcont = resp.read().decode('utf-8')
+agent = 'Mozilla/5.0 (Windows NT 5.1; rv:33.0) Gecko/20100101 Firefox/33.0'
+headers = {
+    'User-Agent': agent
+}
 
-# 解析网页
-soup = BeautifulSoup(htmlcont)
+s = requests.Session()
+r=s.get(loginUrl,allow_redirects=True, headers=headers)
+html = r.text
+# print (loginUrl)
 
-# 中文转拼音库
-p = Pinyin()
+pattern = r'name="_csrf" value="(.*?)"'
+_csrf = re.findall(pattern,html)
 
-tr = soup.find_all('tr')
+datas={
+'_csrf':_csrf[0],
+'LoginForm[username]':username,
+'LoginForm[password]':password,
+'remember':'on',
+}
 
-i=0
-for t in tr:
-    if i == 0:	
-        i += 1
-        continue
-    td = t.find_all('td')
-    print('"'+p.get_initials(td[1].contents[0], u'')+'"'+'=>'+'"'+t.get('data-tb-id')+'"'+','+'    '+'//'+td[1].contents[0])
+if state == '1':
+    zloginUrl = 'http://mp.tmtsp.com/common/safe/login'
+else:
+    zloginUrl = 'http://mp.rmt.test.routeryuncs.com/common/safe/login'
+
+r = s.post(zloginUrl, data=datas, allow_redirects=True, headers=headers)
+# print (r.text)
+
+url = loginUrl+'/client/client-config'
+r = s.get(url,headers=headers)
+# print (r.text)
+
+html = r.text
+pattern = r'class="item" data-key="(.*?)"'
+data_key = re.findall(pattern,html)
+# print(data_key)
+
+url = loginUrl+'/client/manage/index?id='+cstkey
+r = s.get(url, headers=headers)
+html = r.text
+# print (html)
+pattern = r'class = "btn btn-primary br2 btn-xs fs12" href = "(.*?)"'
+curl = re.findall(pattern, html)
+# print (curl)
+for colm in curl:
+
+    url = loginUrl+''+colm
+    r = s.get(url, headers=headers)
+
+    htmlcont = r.text
+
+    # 解析网页
+    soup = BeautifulSoup(htmlcont)
+
+    # 中文转拼音库
+    p = Pinyin()
+
+    tr = soup.find_all('tr')
+
+    i = 0
+
+    print ('==================================================')
+
+    for t in tr:
+        if i == 0:
+            i += 1
+            continue
+        td = t.find_all('td')
+        print('"' + p.get_initials(td[1].contents[0], u'') + '"' + '=>' + '"' + t.get(
+            'data-tb-id') + '"' + ',' + '    ' + '//' + td[1].contents[0])
